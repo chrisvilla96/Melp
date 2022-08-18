ea4157cb5aa98cc0301fd11cebebebc079e9f8c4
Chris <chris96.villarreal@gmail.com>
2022-08-18 00:39:32 -0500

    Create View for Restaurant API

--- Melp/api/views.py
+++ Melp/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class RestaurantView(View):
+    """View for Restaurant API Endpoints"""
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+    
+    def delete(self, request):
+        pass
