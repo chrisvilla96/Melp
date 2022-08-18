d821f9db8fdb9f0e67d568b8ee50834c07872afe
Chris <chris96.villarreal@gmail.com>
2022-08-18 00:35:58 -0500

    Changes for Restaurant Model

--- Melp/api/models.py
+++ Melp/api/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Restaruant(models.Model):
+class Restaurant(models.Model):
     """Model for Restaurant Data"""
 
+    id = models.CharField(primary_key=True, editable=False, max_length=254)
     rating = models.IntegerField(
         default=0, 
         validators = [MinValueValidator(0), MaxValueValidator(5)]
     )
     name = models.CharField(max_length=254)
-    name = models.TextField(max_length=1000)
+    site = models.TextField(max_length=1000)
     email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=13)
     street = models.TextField(max_length=1000)
