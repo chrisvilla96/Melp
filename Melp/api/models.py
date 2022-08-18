6097ae9347f57c568961ab254577d1caa19facd3
Chris <chris96.villarreal@gmail.com>
2022-08-17 23:23:03 -0500

    Create Restaurant Model

--- Melp/api/models.py
+++ Melp/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Restaruant(models.Model):
+    """Model for Restaurant Data"""
+
+    rating = models.IntegerField(
+        default=0, 
+        validators = [MinValueValidator(0), MaxValueValidator(5)]
+    )
+    name = models.CharField(max_length=254)
+    name = models.TextField(max_length=1000)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=13)
+    street = models.TextField(max_length=1000)
+    city = models.CharField(max_length=120)
+    state = models.CharField(max_length=100)
+    lat = models.DecimalField(max_digits=8, decimal_places=3)
+    lng = models.DecimalField(max_digits=8, decimal_places=3)
+
+    def __str__(self) -> str:
+        return self.name
